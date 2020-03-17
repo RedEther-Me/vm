16e43edd89c67337092bba3d71865d680424944b
Andrew Johnson <lordxuqra@gmail.com>
2020-03-16 20:41:30 -0400

    use makefiles

--- packages/assembler/src/index.js
+++ packages/assembler/src/index.js
@@ -1,10 +1,12 @@
 import commander from "commander";
+import yaml from "node-yaml";
 
 import { readFileAsync, writeFileAsync } from "@emulator/core";
 
 import assembler from "./assembler.js";
 
 commander
+  .option("-m, --makefile [folder]", "makefile")
   .option("-s, --source [file].asm", "asm source file")
   .option("-o, --output [file].bin", "output binary file");
 
@@ -23,13 +25,31 @@ function outputName(source, output) {
   return `${base}.bin`;
 }
 
-async function runner() {
-  const { source, output } = commander;
-
+async function assemble(source, output) {
   const input = await readFileAsync(source);
   const result = await assembler(input);
 
   await writeFileAsync(outputName(source, output), result);
 }
 
-runner();
+async function runner() {
+  const { source, output, makefile } = commander;
+
+  if (makefile) {
+    const config = await yaml.read(`${makefile}/makefile`);
+
+    await Object.entries(config).reduce(async (prev, [key, files]) => {
+      await prev;
+      await assemble(`${makefile}/${key}/${files[0]}`);
+      return;
+    }, Promise.resolve());
+  } else {
+    assemble(source, output);
+  }
+}
+
+try {
+  runner();
+} catch (err) {
+  console.log(err);
+}
